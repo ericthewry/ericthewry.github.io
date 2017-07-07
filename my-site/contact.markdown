9943586e486267f9552b5db23eb68f86e8874868
Eric Campbell <1234goal@gmail.com>
2017-07-07 01:30:10 -0700

    Update site

--- my-site/contact.markdown
+++
@@ -1,5 +0,0 @@
----
-title: Contact
----
-
-Email me at eric.hayden.campbell@gmail.com or ehc86@cornell.edu.  You can also call or text me at 909-343-9392.
--- my-site/site.hs
+++ my-site/site.hs
@@ -15,32 +15,12 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
-
     match "papers/*" $ do
         route $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/paper.html"    paperCtx
             >>= loadAndApplyTemplate "templates/default.html" paperCtx
             >>= relativizeUrls
-
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            papers <- recentFirst =<< loadAll "papers/*"
-            let archiveCtx =
-                    listField "papers" paperCtx (return papers) `mappend`
-                    constField "title" "Archives"               `mappend`
-                    defaultContext
-
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
 
 
     match "index.html" $ do
